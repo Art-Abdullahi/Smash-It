785ff93eb0cf13e8ed243b09e198abae6ad5eef5
Your Nameart-abdullahi <abdullahihassanke@gmail.com>
2020-09-05 23:54:21 +0300

    forecast design

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 var input = document.getElementById("search");
 var button = document.getElementById("icon");
-const locationName = document.getElementById("location-name");
-const temparature = document.getElementById("temp");
-const weather = document.getElementById("weather");
+let locationName = document.getElementById("location-name");
+let temparature = document.getElementById("temp");
+let weather = document.getElementById("weather");
+let image = document.getElementById("img");
+let windSpeed = document.getElementById("windspeed");
+let humidity = document.getElementById("humidity");
+let time = document.getElementById("time");
 const converter = (temp) => {
   return Math.floor(temp - 273.15);
+};
+
+const formatter = (datetime) => {
+  const date = new Date(datetime * 1000);
+  const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const day = date.getDay();
+  let hours = date.getHours();
+  const minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  time.innerHTML = days[day - 1] + " at " + hours + ":" + minutes;
 };
 
 button.addEventListener("click", async function search() {
@@ -18,6 +34,10 @@ button.addEventListener("click", async function search() {
     const realTemp = converter(data.main.temp);
     temparature.innerHTML = realTemp + "°C";
     weather.innerHTML = data.weather[0].main;
+    windSpeed.innerHTML = data.wind.speed + "km/h";
+    humidity.innerHTML = data.main.humidity + "%";
+    const time = formatter(data.dt);
+    image.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     console.log(data);
     const secondResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&
     exclude=hourly,daily&appid=a5f4b0fe9b2866e4571b89879fd57c60`);
